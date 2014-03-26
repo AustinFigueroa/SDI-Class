35fb83c6c7ad7f104cdb0c68069ed623d7d6c6af
Austin Figueroa <Austin@Austins-MacBook-Pro.local>
2014-03-26 18:35:07 -0400

    Added another function (number function)

--- sdi-project40092b621-c34f-44e0-9542-17a3ff39159a (1)/rename-me.js
+++ sdi-project40092b621-c34f-44e0-9542-17a3ff39159a (1)/rename-me.js
@@ -33,3 +33,22 @@ var decimal = function (decimals) {
 };
 
 console.log(decimal(2.1));
+
+// Question 3
+// Find the number of hours or days difference between two dates.
+
+var theDate = function (date1,date2) {
+                var results = [];
+                var difference = (date1 > date2) ? date1.getTime() - date2.getTime() : date2.getTime() - date1.getTime();
+                results[3] = difference / 1000;
+                results[2] = results[3] / 60;
+                results[1] = results[2] / 60;
+                results[0] = results[1] / 24;
+                return results;
+                
+    };
+
+var date1 = new Date(2014,3,20);
+var date2 = new Date(2014,4,3);
+var timeConverter = theDate(date1,date2);
+console.log("The difference between the two dates is " + timeConverter[0] + " days.");
