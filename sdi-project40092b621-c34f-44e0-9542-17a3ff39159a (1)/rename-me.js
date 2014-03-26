4c649ae152da1131c12a66319708f060621656ad
Austin Figueroa <Austin@Austins-MacBook-Pro.local>
2014-03-26 18:20:11 -0400

    Added a Function

--- sdi-project40092b621-c34f-44e0-9542-17a3ff39159a (1)/rename-me.js
+++ sdi-project40092b621-c34f-44e0-9542-17a3ff39159a (1)/rename-me.js
@@ -22,3 +22,14 @@ var array = [5, 82, 48, 92, 101, 138];
 var number = 36;
 
 console.log("The smallest value in the array that is greater than " + number + " is " + myValue(array, number) + ".");
+
+// Question 2
+// Format a number to use a specific number of decimal places, as for money: 2.1 --> 2.10
+
+var decimal = function (decimals) {
+        var newDecimal;
+        newDecimal = decimals.toFixed(2);
+        return (decimals + " is rounded to " + newDecimal);        
+};
+
+console.log(decimal(2.1));
